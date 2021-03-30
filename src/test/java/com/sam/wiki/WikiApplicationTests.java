fe8bfe058b02733c8773a2650b09f6e40e4534a6
Sam <samuel3939w@gmail.com>
2021-03-30 16:40:19 +0800

    2-2 init

---
+++ src/test/java/com/sam/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
