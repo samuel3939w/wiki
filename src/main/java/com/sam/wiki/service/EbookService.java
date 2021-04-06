a42ead06f975c12f485e51eddd050fb52de6011f
Sam <samuel3939w@gmail.com>
2021-04-06 11:02:58 +0800

    3-8 製作CopyUtil封裝BeanUtils

--- src/main/java/com/sam/wiki/service/EbookService.java
+++ src/main/java/com/sam/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.sam.wiki.domain.EbookExample;
 import com.sam.wiki.mapper.EbookMapper;
 import com.sam.wiki.req.EbookReq;
 import com.sam.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.sam.wiki.utils.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,19 +16,24 @@ public class EbookService {
     @Autowired
     private EbookMapper ebookMapper;
 
-    public List<EbookResp> list(EbookReq req){
+    public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%"+req.getName()+"%");
+        criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList=new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            respList.add(ebookResp);
-        }
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            //EbookResp ebookResp = new EbookResp();
+//           //BeanUtils.copyProperties(ebook,ebookResp);
+//           //對象複製
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//
+//            respList.add(ebookResp);
+//        }
+        //列表複製
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
 
-        return respList;
+        return list;
     }
 }
