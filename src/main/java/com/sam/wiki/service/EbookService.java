464b53a5062463ed5eb623bca3d07b8a1e387802
Sam <samuel3939w@gmail.com>
2021-04-07 11:10:02 +0800

    5-4 列表查詢接口支持動態SQL name參數有值時按name查, 沒值時查全部 封面小圖樣是調整

--- src/main/java/com/sam/wiki/service/EbookService.java
+++ src/main/java/com/sam/wiki/service/EbookService.java
@@ -8,6 +8,7 @@ import com.sam.wiki.resp.EbookResp;
 import com.sam.wiki.utils.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.List;
 
@@ -19,7 +20,9 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req) {
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        if(!ObjectUtils.isEmpty(req.getName())) {
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
 //        List<EbookResp> respList = new ArrayList<>();
