d2d9e660a229d4e198d0d35fdbfb9fe31113d1ef
Sam <samuel3939w@gmail.com>
2021-04-08 17:12:21 +0800

    6-2 增加電子書管理頁面。 新頁面開發: 頁面，路由，菜單

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -6,15 +6,21 @@
                 mode="horizontal"
                 :style="{ lineHeight: '64px' }"
         >
-            <a-menu-item key="1">nav 1</a-menu-item>
-            <a-menu-item key="2">nav 2</a-menu-item>
-            <a-menu-item key="3">nav 3</a-menu-item>
+            <a-menu-item key="/">
+                <router-link to="/">首頁</router-link>
+            </a-menu-item>
+            <a-menu-item key="/admin/ebook">
+                <router-link to="/admin/ebook">電子書管理</router-link>
+            </a-menu-item>
+            <a-menu-item key="/about">
+                <router-link to="/about">關於我們</router-link>
+            </a-menu-item>
         </a-menu>
     </a-layout-header>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+    import {defineComponent} from 'vue';
 
     export default defineComponent({
         name: 'the-header'
---
+++ web/src/views/admin/admin-ebook.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>電子書管理</h1>
+  </div>
+</template>
