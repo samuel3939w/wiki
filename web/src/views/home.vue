a06986e7b4e7ba251c11b310765ecac2d764d8da
Sam <samuel3939w@gmail.com>
2021-04-14 16:18:29 +0800

    7-7 首頁顯示分類菜單

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -1,58 +1,46 @@
 <template>
     <a-layout-sider width="200" style="background: #fff">
-        <a-menu
-                mode="inline"
-                :style="{ height: '100%', borderRight: 0 }"
-        >
-            <a-sub-menu key="sub1">
-                <template #title>
-              <span>
-                <user-outlined/>
-                subnav 1
-              </span>
+        <a-menu mode="inline" :style="{ height: '100%', borderRight: 0 }" @click="handleClick">
+            <a-menu-item key="welcome">
+                <router-link :to="'/'">
+                    <MailOutlined/>
+                    <span>歡迎</span>
+                </router-link>
+            </a-menu-item>
+            <a-sub-menu v-for="item in level1" :key="item.id">
+                <template v-slot:title>
+                    <span><user-outlined/>{{item.name}}</span>
                 </template>
-                <a-menu-item key="1">option1</a-menu-item>
-                <a-menu-item key="2">option2</a-menu-item>
-                <a-menu-item key="3">option3</a-menu-item>
-                <a-menu-item key="4">option4</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub2">
-                <template #title>
-              <span>
-                <laptop-outlined/>
-                subnav 2
-              </span>
-                </template>
-                <a-menu-item key="5">option5</a-menu-item>
-                <a-menu-item key="6">option6</a-menu-item>
-                <a-menu-item key="7">option7</a-menu-item>
-                <a-menu-item key="8">option8</a-menu-item>
-            </a-sub-menu>
-            <a-sub-menu key="sub3">
-                <template #title>
-              <span>
-                <notification-outlined/>
-                subnav 3
-              </span>
-                </template>
-                <a-menu-item key="9">option9</a-menu-item>
-                <a-menu-item key="10">option10</a-menu-item>
-                <a-menu-item key="11">option11</a-menu-item>
-                <a-menu-item key="12">option12</a-menu-item>
+                <a-menu-item v-for="child in item.children" :key="child.id">
+                    <a-menu-item key="2">option2</a-menu-item>
+                    <MailOutlined/>
+                    <span>{{child.name}}</span>
+                    <a-menu-item key="3">option3</a-menu-item>
+                </a-menu-item>
             </a-sub-menu>
         </a-menu>
     </a-layout-sider>
     <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+            :style="{
+      background: '#fff',
+      padding: '24px',
+      margin: 0,
+      minHeight: '280px',
+    }"
     >
-        <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+        <a-list
+                item-layout="vertical"
+                size="large"
+                :grid="{ gutter: 20, column: 3 }"
+                :data-source="ebooks"
+        >
             <template #renderItem="{ item }">
                 <a-list-item key="item.name">
                     <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px"/>
-            {{ text }}
-          </span>
+                            <span v-for="{ type, text } in actions" :key="type">
+                            <component v-bind:is="type" style="margin-right: 8px"/>
+                               {{ text }}
+                            </span>
                     </template>
                     <a-list-item-meta :description="item.description">
                         <template #title>
@@ -69,8 +57,10 @@
 </template>
 
 <script lang="ts">
-    import {defineComponent, onMounted, ref, reactive, toRef} from 'vue';
-    import axios from 'axios';
+    import {defineComponent, onMounted, ref, reactive, toRef} from "vue";
+    import axios from "axios";
+    import {message} from 'ant-design-vue';
+    import {Tool} from "@/utils/tool";
 
     // const listData: Record<string, string>[] = [];
     //
@@ -87,23 +77,50 @@
     // }
 
     export default defineComponent({
-        name: 'Home',
+        name: "Home",
         setup() {
             const ebooks = ref();
-            const ebooks1 = reactive({books: []});
+            // const ebooks1 = reactive({books: []});
+
+            const level1 = ref();
+            let categorys: any;
+            /**
+             * 查询所有分类
+             **/
+            const handleQueryCategory = () => {
+                axios.get("/category/all").then((response) => {
+                    const data = response.data;
+                    if (data.success) {
+                        categorys = data.content;
+                        console.log("原始数组：", categorys);
+
+                        level1.value = [];
+                        level1.value = Tool.array2Tree(categorys, 0);
+                        console.log("树形结构：", level1.value);
+                    } else {
+                        message.error(data.message);
+                    }
+                });
+            };
+
+            const handleClick = () => {
+                console.log("menu click")
+            };
 
             onMounted(() => {
-                axios.get("/ebook/list",{
-                    params:{
-                        page:1,
-                        size:1000
-                    }
-                }).then((response) => {
-                    const data = response.data;
-                    ebooks.value = data.content.list;
-                    //ebooks1.books = data.content;
+                handleQueryCategory();
+                axios.get("/ebook/list", {
+                    params: {
+                        page: 1,
+                        size: 1000,
+                    },
                 })
-            })
+                    .then((response) => {
+                        const data = response.data;
+                        ebooks.value = data.content.list;
+                        //ebooks1.books = data.content;
+                    });
+            });
 
             const pagination = {
                 onChange: (page: number) => {
@@ -112,17 +129,20 @@
                 pageSize: 3,
             };
             const actions: Record<string, string>[] = [
-                {type: 'StarOutlined', text: '156'},
-                {type: 'LikeOutlined', text: '156'},
-                {type: 'MessageOutlined', text: '2'},
+                {type: "StarOutlined", text: "156"},
+                {type: "LikeOutlined", text: "156"},
+                {type: "MessageOutlined", text: "2"},
             ];
 
             return {
                 ebooks,
                 pagination,
                 actions,
+                level1,
+
+                handleClick
             };
-        }
+        },
     });
 </script>
 
