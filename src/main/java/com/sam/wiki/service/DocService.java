00a64df779c109784b365f3458aaea2bdb8203e5
Sam <samuel3939w@gmail.com>
2021-04-29 15:07:08 +0800

    10-10 將業務線程日誌流水號傳遞到異部線程, 方便生產運維查問題

--- src/main/java/com/sam/wiki/service/DocService.java
+++ src/main/java/com/sam/wiki/service/DocService.java
@@ -20,6 +20,7 @@ import com.sam.wiki.utils.RequestContext;
 import com.sam.wiki.utils.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -156,7 +157,8 @@ public class DocService {
 
         //推送消息
         Doc docDb = docMapper.selectByPrimaryKey(id);
-        wsService.sendInfo("【" + docDb.getName() + "】被點讚!");
+        String logId = MDC.get("LOG_ID");
+        wsService.sendInfo("【" + docDb.getName() + "】被點讚!",logId);
     }
 
     public void updateEbookInfo() {
--- src/main/java/com/sam/wiki/service/WsService.java
+++ src/main/java/com/sam/wiki/service/WsService.java
@@ -1,6 +1,7 @@
 package com.sam.wiki.service;
 
 import com.sam.wiki.websocket.WebSocketServer;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,8 @@ public class WsService {
     private WebSocketServer webSocketServer;
 
     @Async
-    public void sendInfo(String message) {
+    public void sendInfo(String message,String logId) {
+        MDC.put("LOG_ID",logId);
         webSocketServer.sendInfo(message);
     }
 }
