a2a12ec04e60bc722a70069e0034cf5d45759678
Sam <samuel3939w@gmail.com>
2021-05-04 15:38:43 +0800

    11-6 後端增加獲取統計數值接口

--- src/main/java/com/sam/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/sam/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,7 +1,12 @@
 package com.sam.wiki.mapper;
 
+import com.sam.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
 
+    List<StatisticResp> getStatistic();
 }
--- src/main/java/com/sam/wiki/service/EbookSnapshotService.java
+++ src/main/java/com/sam/wiki/service/EbookSnapshotService.java
@@ -1,9 +1,11 @@
 package com.sam.wiki.service;
 
 import com.sam.wiki.mapper.EbookSnapshotMapperCust;
+import com.sam.wiki.resp.StatisticResp;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class EbookSnapshotService {
@@ -12,5 +14,12 @@ public class EbookSnapshotService {
 
     public void genSnapshot() {
         ebookSnapshotMapperCust.genSnapshot();
+    }
+
+    /**
+     * 获取首页数值数据：总阅读数、总点赞数、今日阅读数、今日点赞数、今日预计阅读数、今日预计阅读增长
+     */
+    public List<StatisticResp> getStatistic() {
+        return ebookSnapshotMapperCust.getStatistic();
     }
 }
