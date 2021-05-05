6779254f4d6db8b300a9b6c83be5097b6dfae637
Sam <samuel3939w@gmail.com>
2021-05-05 12:13:57 +0800

    11-9 關於我們, 文案修改

--- web/src/views/about.vue
+++ web/src/views/about.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>關於我們</h1>
-  </div>
+  <a-layout>
+    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+      <div class="about">
+        <h2>我是山姆，1年前端經驗</h2>
+        <div><b>專注於前端技術研究</b></div>
+        <div>
+          <b>項目實戰：
+              《SpringBoot知識體系+Vue3全家桶 前後端分離 實戰WIKI知識庫系统》
+          </b>
+        </div>
+
+        <br>
+        <div><b>微信號：sam123</b></div>
+      </div>
+    </a-layout-content>
+  </a-layout>
 </template>
+
+<style>
+  .about {
+    line-height: 30px;
+    padding: 40px;
+    background-color: #f5f5f5;
+    font-size: 16px;
+  }
+  .about h2 {
+    margin-bottom: 30px;
+    font-weight: 700;
+  }
+  .about img {
+    margin-top: 10px;
+  }
+</style>
